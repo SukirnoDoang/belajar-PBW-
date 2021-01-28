5e2db9b4e7e270e2c5e8e49e0bcdc6a6dac43494
MarioBrossBlack <crossholly4872@gmail.com>
2021-01-28 13:11:28 +0700

    belajar validasi

--- biodata.php
+++ biodata.php
@@ -6,7 +6,7 @@
     <title>Biodata</title>
 </head>
 <body>
-    <form action="hasil_biodata.php" method="POST">
+    <form action="hasil_biodata.php" method="POST" id="form-biodata">
         <table>
             <tr>
                 <td colspan="3" class="text-center">Biodata </td>
@@ -40,11 +40,26 @@
             </tr>
             <tr>
                 <td colspan="3" align="right">
-                   <button type="submit">Submit </button>
+                   <button type="button" onclick = "validasi()">Submit </button>
                    <button type="reset">Clear </button>
                 </td>
             </tr>
         </table>
     </form>
 </body>
+
+<script type="text/javascript">
+    function validasi(){
+        var npm = document.getElementById('npm').value;
+        var nama = document.getElementById('nama').value;
+        if (npm == "") {
+            alert('NPM Belum Diisi,.');
+        } else if(nama == "") {
+            alert('Nama Belum Diisi,.');
+        } else {
+            document.getElementById('form-biodata').submit()
+        }
+    }
+</script>
+
 </html>
